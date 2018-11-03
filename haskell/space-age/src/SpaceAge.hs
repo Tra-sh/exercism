1ace114ab08ba71705c6f45afa0c724d7fd4f4df
Vladimir Logachev <doit@keemail.me>
2018-11-03 19:29:40 +0700

    fix indentation

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -10,14 +10,15 @@ data Planet = Mercury
             | Neptune
 
 period :: Planet -> Float
-period p = case p of Earth -> 31557600 -- 365.25 Earth days, or 31557600 seconds
-                     Mercury -> 0.2408467 * period Earth
-                     Venus -> 0.61519726 * period Earth
-                     Mars -> 1.8808158 * period Earth
-                     Jupiter -> 11.862615 * period Earth
-                     Saturn -> 29.447498 * period Earth
-                     Uranus -> 84.016846 * period Earth
-                     Neptune -> 164.79132 * period Earth
+period p = case p of 
+    Earth -> 31557600 -- 365.25 Earth days, or 31557600 seconds
+    Mercury -> 0.2408467 * period Earth
+    Venus -> 0.61519726 * period Earth
+    Mars -> 1.8808158 * period Earth
+    Jupiter -> 11.862615 * period Earth
+    Saturn -> 29.447498 * period Earth
+    Uranus -> 84.016846 * period Earth
+    Neptune -> 164.79132 * period Earth
 
 ageOn :: Planet -> Float -> Float
 ageOn planet seconds = seconds / period planet
